fbe34e71ebeb38f029eb37920d7de40bb36d2d07
Kimkyungmin123 <rud33023333@gmail.com>
2022-03-10 20:37:05 +0900

    data 삽입

--- src/components/certificate/Certificate.jsx
+++ src/components/certificate/Certificate.jsx
@@ -2,8 +2,35 @@ import './certificate.scss';
 
 export default  function Certificate ()  {
 
+
+    const list = [
+        {
+            id : 1,
+            date: "2021. 08. 20 ",
+            title : "정보처리기사",
+            inst : "한국산업인력공단",
+        },
+        {
+            id : 2,
+            date: "2020. 11. 08",
+            title : "TOEIC SPEAKING",
+            inst : " level6 - ETS",
+        },
+    ];
+
     return (
         <div className='certificate' id='certificate'>
+            <h1>Certificate</h1>
+            
+                {list.map((data)=>(
+                    <div>
+                        <h2>{data.title}</h2>
+                        <h3>{data.date}</h3>
+                        <h4>{data.inst}</h4>
+                    </div>
+                    ))}
+            
+            
             
         </div>
     );
--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -2,15 +2,46 @@ import { useState } from 'react';
 import './education.scss';
 
 export default  function Education ()  {
-    const [message, setmMessage] = useState(false);
 
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        setmMessage(true);
-    } 
+    const list = [
+        {
+            id : 1,
+            date: "2021. 08",
+            title : "99일 완주반 프론트엔드 개발자되기",
+            inst : "제로베이스",
+        },
+        {
+            id : 2,
+            date: "2021. 06",
+            title : "김민태의 프론트엔드아카데미",
+            inst : " 패스트캠퍼스",
+        },
+        {
+            id : 3,
+            date: "2021. 02",
+            title : "산업공학부 디자인공학전공 졸업",
+            inst : "금오공과대학교",
+        },
+        {
+            id : 4,
+            date: "2021. 01",
+            title : "HTML/CSS/JavsScript 기초",
+            inst : "인프런",
+        },
+    ];
+
+
     return (
         <div className='education' id='education'>
-         
+            <h1>Education</h1>
+
+            {list.map((data)=>(
+                <div>
+                <h3>{data.date}</h3>
+                <h2>{data.title}</h2>
+                <h4>{data.inst}</h4>
+                </div>
+            ))}
         </div>
     );
 }
