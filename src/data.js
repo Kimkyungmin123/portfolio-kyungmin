047b7208822fd675ac4c4f282edd8369eaf56a00
Kimkyungmin123 <rud33023333@gmail.com>
2022-03-11 19:55:02 +0900

    mobile ver. style수정,플젝 gif

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const moviePortfolio = [
     {
         id:1,
         title:"Movie App",
-        img:"https://s3.us-west-2.amazonaws.com/secure.notion-static.com/0783f467-923c-4f2c-8396-65e811c8dbb8/%EC%BA%A1%EC%B2%98.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220310%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220310T093215Z&X-Amz-Expires=86400&X-Amz-Signature=2c863896a7d4ff65a39e096154c2d17c58776ecb61973388388fbca22fb320df&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22%25EC%25BA%25A1%25EC%25B2%2598.JPG.jpg%22&x-id=GetObject"
+        img:"https://s3.us-west-2.amazonaws.com/secure.notion-static.com/ebb10506-72c8-4116-913c-75c445b1fecc/ezgif.com-gif-maker_%285%29.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220311%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220311T103903Z&X-Amz-Expires=86400&X-Amz-Signature=46b840aafb609d8bdca9bbcc49adf2f30a617d7f3c744f9c52b62945d39b4e23&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22ezgif.com-gif-maker%2520%285%29.gif%22&x-id=GetObject"
     },
     
 
@@ -12,7 +12,7 @@ export const paintPortfolio = [
     {
         id:1,
         title:"paint",
-        img:"https://s3.us-west-2.amazonaws.com/secure.notion-static.com/7a1e8de6-abe8-49de-ae28-fe730ca65009/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220310%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220310T092120Z&X-Amz-Expires=86400&X-Amz-Signature=e24adca4efea35a3fe86d0f982b3b093cbcee7874ba6296dd459e68186399f01&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Untitled.png%22&x-id=GetObject"
+        img:"https://s3.us-west-2.amazonaws.com/secure.notion-static.com/37be4af6-c234-442f-9efd-b48db27aded5/ezgif.com-gif-maker_%286%29.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220311%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220311T103952Z&X-Amz-Expires=86400&X-Amz-Signature=06c43342239c5fe86b5d36ca1a719194b45ae405a6e8b1508672a093b14a8db4&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22ezgif.com-gif-maker%2520%286%29.gif%22&x-id=GetObject"
     },
     
 ]
@@ -21,7 +21,7 @@ export const minishopPortfolio = [
     {
         id:1,
         title:"minishop",
-        img:"https://s3.us-west-2.amazonaws.com/secure.notion-static.com/eb3d3c0e-3025-4016-8de8-6db3018684a8/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220310%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220310T092245Z&X-Amz-Expires=86400&X-Amz-Signature=0ff0ad8e2c601712d5f931d91f4a9cbb9d96bc76463f707ff3d6b24cf8178245&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Untitled.png%22&x-id=GetObject"
+        img:"https://s3.us-west-2.amazonaws.com/secure.notion-static.com/1e7df807-6c1b-4957-a440-99ab5049feda/ezgif.com-gif-maker_%284%29.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220311%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220311T103756Z&X-Amz-Expires=86400&X-Amz-Signature=504274f6cb157571258badfd5c00d5a26aba70f20e2491b5a6fb1ad0abf8aa04&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22ezgif.com-gif-maker%2520%284%29.gif%22&x-id=GetObject"
     },
     
 
@@ -32,7 +32,7 @@ export const todolistPortfolio = [
     {
         id:1,
         title:"todolist",
-        img:"https://user-images.githubusercontent.com/70140547/144764073-164858e4-9520-45e1-941c-93d399a9736b.JPG"
+        img:"https://s3.us-west-2.amazonaws.com/secure.notion-static.com/b24721e2-893b-43d9-b1ef-acadfe37c865/ezgif.com-gif-maker_%283%29.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220311%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220311T104039Z&X-Amz-Expires=86400&X-Amz-Signature=00ef35ab05b384af10db8da7441cbeb7677ed80ce509cfe51925a38aecf635b2&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22ezgif.com-gif-maker%2520%283%29.gif%22&x-id=GetObject"
     },
     
     
